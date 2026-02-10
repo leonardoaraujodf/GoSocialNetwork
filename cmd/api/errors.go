656118ad58ffc50cc705e073ac4491cfecba3155
Leonardo Amorim <leonardoaraujodf@gmail.com>
2026-02-10 12:49:42 -0300

    feat: Start versioning social network project in Go
    Signed-off-by: Leonardo Amorim <leonardoaraujodf@gmail.com>

---
+++ cmd/api/errors.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("internal server error: %s path: %s error: %v", r.Method, r.URL.Path, err)
+	WriteJSONError(w, http.StatusInternalServerError, "the server encountered a problem")
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("bad request error: %s path: %s error: %v", r.Method, r.URL.Path, err)
+	WriteJSONError(w, http.StatusBadRequest, err.Error())
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("not found response: %s path: %s error: %v", r.Method, r.URL.Path, err)
+	WriteJSONError(w, http.StatusNotFound, "not found")
+}
