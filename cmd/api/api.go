99da7461e0ac405b68ca710f33c304facf6d4612
Leonardo Amorim <leonardoaraujodf@gmail.com>
2026-02-18 11:42:32 -0300

    feat: Start adding support for authentication

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -35,6 +35,11 @@ type config struct {
 	env     string
 	version string
 	apiURL  string
+	mail    mailConfig
+}
+
+type mailConfig struct {
+	exp time.Duration
 }
 
 func (app *application) mount() http.Handler {
@@ -79,6 +84,11 @@ func (app *application) mount() http.Handler {
 			r.Group(func(r chi.Router) {
 				r.Get("/feed", app.getUserFeedHandler)
 			})
+
+		})
+		// Public routes
+		r.Route("/authentication", func(r chi.Router) {
+			r.Post("/user", app.registerUserHandler)
 		})
 	})
 
--- internal/db/seed.go
+++ internal/db/seed.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"math/rand"
@@ -130,17 +131,18 @@ var comments = []string{
 	"Your writing style makes complex topics easy to understand.",
 }
 
-func Seed(store store.Storage) {
+func Seed(store store.Storage, db *sql.DB) {
 	ctx := context.Background()
 
 	users := generateUsers(100)
+	tx, _ := db.BeginTx(ctx, nil)
 	for _, user := range users {
-		if err := store.Users.Create(ctx, user); err != nil {
+		if err := store.Users.Create(ctx, tx, user); err != nil {
 			log.Println("Error creating user:", err)
 			return
 		}
 	}
-
+	tx.Commit()
 	posts := generatePosts(200, users)
 	for _, post := range posts {
 		if err := store.Posts.Create(ctx, post); err != nil {
@@ -166,7 +168,6 @@ func generateUsers(num int) []*store.User {
 		users[i] = &store.User{
 			Username: usernames[i%len(usernames)] + "_" + fmt.Sprintf("%d", i),
 			Email:    usernames[i%len(usernames)] + "_" + fmt.Sprintf("%d", i) + "@example.com",
-			Password: "12345",
 		}
 	}
 	return users
