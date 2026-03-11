72d80a0cfaf2f4b0a20158ae8ff632d067993d3b
Leonardo Amorim <leonardoaraujodf@gmail.com>
2026-03-11 18:43:39 -0300

    feat: Add basic front-end to handle token confirmation

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"go.uber.org/zap"
 
 	// "github.com/leonardoaraujodf/social/docs"
@@ -54,6 +55,25 @@ type sendGridConfig struct {
 
 func (app *application) mount() http.Handler {
 	r := chi.NewRouter()
+
+	// Basic CORS
+	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+
+	http.ListenAndServe(":3000", r)
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
